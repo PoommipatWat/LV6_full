efb18f263bde3ecf631985e32c2253c8aef6a4a7
Poommipat Wattanaprasit <95070986+PoommipatWat@users.noreply.github.com>
2025-09-28 12:47:48 +0700

    Add files via upload

---
+++ lv4/lcd.cpp
@@ -0,0 +1,29 @@
+#include <LiquidCrystal_I2C.h> 
+__________________
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+__________________
+/*-----------------------------------------------*/
+void LcdSetup()
+{
+  lcd.init();
+  lcd.display();        //เปิดหน้าจอ
+  lcd.backlight();      //เปิดไฟ backlight
+  lcd.clear();          //ล้างหน้าจอ
+  DisplayLcd(5,"Level4",3,"FWV 1.0.0");
+}
+/*-----------------------------------------------*/
+void DisplayLcd(uint8_t Row1,String Text1,uint8_t Row2,String Text2)
+{
+  lcd.clear();
+  lcd.setCursor(Row1,0); 
+  lcd.print(Text1); 
+  lcd.setCursor(Row2,1); 
+  lcd.print(Text2); 
+}
+/*-----------------------------------------------*/
+void DisplayOff() 
+{
+  lcd.clear();
+}
+/*-----------------------------------------------*/
---
+++ lv5/main.cpp
@@ -0,0 +1,29 @@
+//Arduino mega
+#include <avr/wdt.h>
+__________________
+#include <Wire.h>
+
+__________________
+__________________
+/////
+uint32_t Data = 0;
+
+void setup() {
+/*-----------------------------------------------*///Uart
+  Serial.begin(9600);
+  Serial1.begin(9600);//Level1-2
+  Serial2.begin(9600);//Level4
+  Oled1306Setup();
+  Serial.println("Level 5 Ready");
+  Serial.println("FWV 1.0.0");
+}
+
+void loop() {
+  OLED();
+}
+
+void resetFunc() { // Restart
+  wdt_disable();
+  wdt_enable(WDTO_15MS);
+  while (1) {}
+}
